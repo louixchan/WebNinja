05f77e38cfa594583c4cb0edda7a6784958d7263
Louis Chan <louis.chan@fticonsulting.com>
2017-11-14 16:28:32 +0000

    switching to htmlPy

--- main.py
+++ main.py
@@ -4,12 +4,25 @@ from PyQt5.QtWidgets import QApplication , QMainWindow
 from testing import *
 from WebNinjaBrowser import *
 from WebNinja import checkDependencies
+import htmlPy
 
 if __name__ == '__main__':
-	checkDependencies()
-	app = QApplication(sys.argv)    
-	mainWindow = QMainWindow()    
-	ui = WebNinjaBrowser()
-	ui.setupUi(mainWindow)    
-	mainWindow.show()    
-	sys.exit(app.exec_()) 
+	import htmlPy
+	import os
+
+	app = htmlPy.AppGUI(title=u"htmlPy Quickstart", maximized=True)
+
+	app.template_path = os.path.abspath(".")
+	app.static_path = os.path.abspath(".")
+
+	app.template = ("index.html", {"username": "htmlPy_user"})
+
+	app.start()
+
+	# checkDependencies()
+	# app = QApplication(sys.argv)
+	# mainWindow = QMainWindow()
+	# ui = WebNinjaBrowser()
+	# ui.setupUi(mainWindow)
+	# mainWindow.show()
+	# sys.exit(app.exec_())
